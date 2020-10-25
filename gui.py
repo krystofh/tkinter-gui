dc8cb7f1fe98e5f4d6cc1116183b740c5a93e93f
krystofh <krystof.hes@seznam.cz>
2020-10-25 13:02:21 +0100

    Updated padding etc.

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-# Sampel demo of GUI Development with Tkinter
+# Sample demo of GUI Development with Tkinter
 # Conversion calculator between imperial and metric units
 # Created by: Krystof Hes
 # 25.10.2020
@@ -24,28 +24,31 @@ def key_pressed(event):
 # Create a new window
 window = tk.Tk()
 window.title("Imperial-SI Units converter")
-frm_entry = tk.Frame(master = window)  # frame(panel) inside the window
+main_frame = tk.Frame(master = window)  # frame(panel) inside the window
+
+#main_frame.rowconfigure(0, minsize = 80, weight = 1)   # Use if minimum size needs to be configured, weight for scaling
+#main_frame.columnconfigure(1, minsize = 100, weight = 1)
 
 # Entry (Text Area) for input
-ent_unit = tk.Entry(master=frm_entry, width = "5")
-ent_unit.grid(row=0, column=0, padx=5, pady = 10)
+ent_unit = tk.Entry(master=main_frame, width = 15)
+ent_unit.grid(row=0, column=0, padx=(15, 2), pady = 7)
 
-lbl_imperial_unit = tk.Label(master=frm_entry, text= "inch")
-lbl_imperial_unit.grid(row=0, column=1, padx=5, pady = 10)
+lbl_imperial_unit = tk.Label(master=main_frame, text= "inch")
+lbl_imperial_unit.grid(row=0, column=1, padx=(2, 5), pady = 10)
 
 # Button for Conversion
 #            Binding the button to a function:
 #          a) attribute command = fcn_name, then the function has no extra argument
 #          b) btn.bind("<Button-1>", fcn_name), then argument "event" needed
-btn_convert = tk.Button(master=frm_entry, text = "\N{RIGHTWARDS BLACK ARROW}", width = "3", height = "1")
-btn_convert.grid(row=0, column=2, padx=10, pady = 10)
+btn_convert = tk.Button(master=main_frame, text = "\N{RIGHTWARDS BLACK ARROW}", width = "3", height = "1")
+btn_convert.grid(row=0, column=2, padx=(20,20), pady = 10)
 btn_convert.bind("<Button-1>", convert_to_cm)
 
-txt_result = " cm"
-lbl_result = tk.Label(master=frm_entry, text=txt_result)
-lbl_result.grid(row=0, column=3, padx=10, pady = 10)
+txt_result = "0 cm"
+lbl_result = tk.Label(master=main_frame, text=txt_result)
+lbl_result.grid(row=0, column=3, padx=(10,15), pady = 10)
 
 # Add frame and create main loop for event handling
-frm_entry.pack()
+main_frame.pack()
 window.bind("<Key>", key_pressed)  # key-event handling
 window.mainloop()
